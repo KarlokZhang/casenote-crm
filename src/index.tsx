d6115738d880e7e9047b36cb9bf0f2edef71b5d7
KarlokCheung <georgecheung07@gmail.com>
2021-09-08 20:42:13 +1000

    config styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'normalize.css/normalize.css';
+import { createGlobalStyle } from 'styled-components';
 import App from './App';
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+  html, body, #app {
+    height: 100%;
+    width: 100%;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
