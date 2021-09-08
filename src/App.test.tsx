a0998655961b0b60bda07080b1f447c723615a69
KarlokCheung <georgecheung07@gmail.com>
2021-09-08 17:36:43 +1000

    initial commit, set up react app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('render app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('react app');
   expect(linkElement).toBeInTheDocument();
 });
