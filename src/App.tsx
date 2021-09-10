1f11bc611c7c2c4f2504337b1bbb25dda5896bff
KarlokCheung <georgecheung07@gmail.com>
2021-09-10 15:55:54 +1000

    add Sidebar Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-function App() {
+import React from 'react';
+import styled from 'styled-components';
+import Sidebar from './components/Sidebar';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  height: 100vh;
+  background-color: #f4f7fe;
+`;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <h2>react app after Deploy</h2>
-    </div>
+    <Wrapper className="App">
+      <Sidebar />
+      <div>react app after Deploy</div>
+    </Wrapper>
   );
-}
+};
 
 export default App;
