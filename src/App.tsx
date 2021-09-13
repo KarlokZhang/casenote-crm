98c22ef7a5137d4ff106d5314ef1064b8872a6a6
KarlokCheung <georgecheung07@gmail.com>
2021-09-13 21:30:01 +1000

    added page layout of sidebar, header and content page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Sidebar from './components/Sidebar';
 import Content from './components/Content';
+import Header from './components/Header';
 
 const Wrapper = styled.div`
   display: flex;
@@ -10,11 +11,20 @@ const Wrapper = styled.div`
   background-color: #f4f7fe;
 `;
 
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
 const App: React.FC = () => {
   return (
     <Wrapper className="App">
       <Sidebar />
-      <Content />
+      <ContentWrapper>
+        <Header />
+        <Content />
+      </ContentWrapper>
     </Wrapper>
   );
 };
--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+  width: 100%;
   display: flex;
   flex: 1;
 `;
